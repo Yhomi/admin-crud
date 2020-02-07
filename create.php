ea7be94a8cd6c29ac45fcd106a41d80b9173dbe9
Yhomi <kareemyomi91@gmail.com>
2020-02-07 13:04:48 +0100

    changes to the project files

--- create.php
+++ create.php
@@ -1,5 +1,5 @@
-<?php include "inc/header" ?>
+<?php include "inc/header.php" ?>
 <div class="container">
     <h1>Create page</h1>
 </div>
-<?php include "inc/footer" ?>
+<?php include "inc/footer.php" ?>
--- show.php
+++ show.php
@@ -1,5 +1,5 @@
-<?php include "inc/header" ?>
+<?php include "inc/header.php" ?>
 <div class="container">
     <h1>Show Page</h1>
 </div>
-<?php include "inc/footer" ?>
+<?php include "inc/footer.php" ?>
